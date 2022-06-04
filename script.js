f872ffd54c4481353890662a18c6383bd63508da
John Bautista <jnl.bautista.cs@gmail.com>
2022-06-04 13:44:14 +0100

    update

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //user inputs 10x10 e.g.
-let userInput = 10;
+let userInput = 20;
 
 let noOfColumns = userInput;
 let pixelHeight = 700 / noOfColumns;
@@ -12,15 +12,39 @@ divsContainer.setAttribute(
 );
 
 //creating pixels/divs
-
 for (let i = 1; i < area + 1; i++) {
-  //div/pixel creation
+  // div/pixel creation
   const div = document.createElement("div");
   div.className = "div" + i.toString();
 
-  //set div height
-  div.setAttribute("style", "background: red; width:" + pixelHeight + "px;");
+  //set div/pixel height
+  div.setAttribute("style", "background: white; width:" + pixelHeight + "px;");
 
   //append
   divsContainer.appendChild(div);
 }
+
+const divsContainer1 =
+  document.getElementsByClassName("divs-container").childNodes;
+  
+
+
+  console.log(divsContainer1);
+
+
+  divsContainer1.forEach((element) => {
+  // and for each one we add a 'click' listener
+  element.addEventListener("mouseover", function () {
+    alert(element.class);
+  });
+});
+
+//mouser over
+
+// const divsContainer1 = document.querySelector(".divs-container");
+// divsContainer1.forEach((element) => {
+//   element.addEventListener("mouseover", function () {
+//       element.setAttribute("style", "background: red;");
+
+//   });
+// });
