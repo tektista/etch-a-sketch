a5ec1808b57b1f3a80a6c71e1711446bb53acfbd
John Bautista <jnl.bautista.cs@gmail.com>
2022-06-04 23:14:05 +0100

    project completed

--- script.js
+++ script.js
@@ -1,44 +1,46 @@
-//user inputs 10x10 e.g.
-let userInput = 20;
+//Default user input of 6
+createGrid(6);
 
-let noOfColumns = userInput;
-let pixelHeight = 700 / noOfColumns;
-let area = noOfColumns * noOfColumns;
 
-const divsContainer = document.querySelector(".divs-container");
-divsContainer.setAttribute(
-  "style",
-  "grid-template-columns: repeat(" + noOfColumns + "," + pixelHeight + "px);"
-);
-
-//creating pixels/divs
-for (let i = 1; i < area + 1; i++) {
-  // div/pixel creation
-  const div = document.createElement("div");
-  div.className = "div" + i.toString();
-
-  //set div/pixel height
-  div.setAttribute("style", "background: white; width:" + pixelHeight + "px;");
-
-  //append
-  divsContainer.appendChild(div);
+function changeDimension() {
+  userInputx = prompt("What size grid do you want? e.g. 64 for 64x64");
+  createGrid(userInputx);
 }
 
-// get all children of .divs-container
-const divsContainer1 = document.querySelector(".divs-container").childNodes;
+function createGrid(userInput) {
+  let noOfColumns = userInput;
+  let pixelHeight = 700 / noOfColumns;
+  let area = noOfColumns * noOfColumns;
 
-divsContainer1.forEach((element) => {
-  element.addEventListener("mouseover", function () {
-    element.setAttribute("style", "background: red;");
+  let divsContainer = document.querySelector(".divs-container");
+  divsContainer.innerHTML = "";
+  divsContainer.setAttribute(
+    "style",
+    "grid-template-columns: repeat(" + noOfColumns + "," + pixelHeight + "px);"
+  );
+
+  //creating pixels/divs
+  for (let i = 1; i < area + 1; i++) {
+    // div/pixel creation
+    let div = document.createElement("div");
+    div.className = "div" + i.toString();
+
+    //set div/pixel height
+    div.setAttribute(
+      "style",
+      "background: white; width:" + pixelHeight + "px;"
+    );
+
+    //append
+    divsContainer.appendChild(div);
+  }
+
+  // get all children of .divs-container
+  let divsContainer1 = document.querySelector(".divs-container").childNodes;
+
+  divsContainer1.forEach((element) => {
+    element.addEventListener("mouseover", function () {
+      element.setAttribute("style", "background: red;");
+    });
   });
-});
-
-//mouser over
-
-// const divsContainer1 = document.querySelector(".divs-container");
-// divsContainer1.forEach((element) => {
-//   element.addEventListener("mouseover", function () {
-//       element.setAttribute("style", "background: red;");
-
-//   });
-// });
+}
